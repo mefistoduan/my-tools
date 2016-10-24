5322b88078a3b9a3b584b7655157217b553fe3b7
mefistoduan <838560574@qq.com>
2016-10-24 22:45:47 +0800

    learn some canvas skill ~

--- tools/h5/GhostInTheShell/js/main.js
+++ tools/h5/GhostInTheShell/js/main.js
@@ -3,3 +3,26 @@
  * @description:
  * @Date: 2016/10/17 14:13
  */
+var can;
+var ctx;
+
+function init(){
+    can = document.getElementById('canvas');
+    ctx = can.getContext('2d');
+
+    w = can.width;
+    h = can.height;
+};
+
+$(function(){
+    init();
+});
+
+function  gameloop(){
+    drawBackground();
+};
+
+function  drawBackground(){
+    ctx.fillStyle = '#393550';
+
+}
